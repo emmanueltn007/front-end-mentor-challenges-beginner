e971bb9642a10c62c39a58be58f53d4135a64cde
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-02-09 17:56:47 +0200

    Added the header and hero section

---
+++ base-apparrel-coming-soon/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header () {
+    return (
+        <header>
+            <img src="/images/logo.svg" alt="base apparrel logo" />
+        </header>
+    );
+}
+
+export default Header
---
+++ base-apparrel-coming-soon/src/components/Hero.jsx
@@ -0,0 +1,9 @@
+function Hero () {
+    return (
+        <section className="h-full w-full">
+            <img className="object-cover h-full w-full" src="/images/hero-mobile.jpg" alt="hero image" />
+        </section>
+    );
+}
+
+export default Hero
