aae26785512b20b681e47872690c78370ad4c3d6
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-02-21 20:46:31 +0200

    commit of the base-apparrel-coming-soon, added new challenge blog-preview-card

--- base-apparel-coming-soon/src/components/Form.jsx
+++ base-apparel-coming-soon/src/components/Form.jsx
@@ -8,7 +8,7 @@ function Form() {
       <input
         onChange={(e) => setEmail(e.target.value)}
         value={email}
-        className={`placeholder:text-[hsl(0,36%,70%)] text-[hsl(0,0%,0%)] font-semibold w-full outline-none border-2  py-3 px-4 rounded-3xl ${error ? "border-[hsl(0,93%,68%)]" : "border-[hsl(0,36%,70%)]"}`}
+        className={`placeholder:text-[hsl(0,36%,70%)] text-[hsl(0,0%,0%)] font-semibold w-full outline-none border-2  py-3 px-4 rounded-3xl caret-[hsl(0,36%,70%)] ${error ? "border-[hsl(0,93%,68%)]" : "border-[hsl(0,36%,70%)]"}`}
         type="text"
         placeholder="email address"
       />
@@ -21,7 +21,7 @@ function Form() {
         </p>
       )}
       <button
-        className="bg-[linear-gradient(90deg,hsl(0,80%,86%),hsl(0,74%,74%))] hover:bg-[linear-gradient(90deg,hsl(0,80%,86%),hsl(0,80%,86%))] transition-all duration-300 py-4 px-8 rounded-3xl cursor-pointer absolute top-0 bottom-0 -translate-x-full"
+        className="bg-[linear-gradient(135deg,hsl(0,80%,86%),hsl(0,74%,74%))] hover:bg-[linear-gradient(135deg,hsl(0,80%,86%),hsl(0,80%,86%))] transition-all duration-300 ease-in-out py-4 px-8 rounded-3xl cursor-pointer absolute top-0 bottom-0 -translate-x-full shadow-xl shadow-[hsl(0,80%,86%)] hover:shadow-[hsl(0,74%,74%)]"
         type="submit"
       >
         <svg xmlns="http://www.w3.org/2000/svg" width="12" height="20">
